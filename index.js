55b442588f9af6665207d280d9c61b7f996c6ccd
Tanmoy Halder <159239492+Tan3oy@users.noreply.github.com>
2025-07-02 11:05:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`server is running at http://localhost:${PORT}`);
+  console.log(`server is running`);
 });
